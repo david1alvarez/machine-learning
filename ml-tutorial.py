fbbb1b29abdee5c9087fb2a0de923f177f28aa75
David Alvarez <daveyalvarez03@gmail.com>
2020-06-03 11:38:08 -0700

    iris tutorial

--- ml-tutorial.py
+++ ml-tutorial.py
@@ -21,30 +21,6 @@ url = "./iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
-# # shape
-# print(dataset.shape)
-
-# # head
-# print(dataset.head(20))
-
-# # descriptions
-# print(dataset.describe())
-
-# # class distribution
-# print(dataset.groupby('class').size())
-
-# # box and whisker plots 
-# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-# pyplot.show()
-
-# # histograms
-# dataset.hist()
-# pyplot.show()
-
-# # scatter plot matrix
-# scatter_matrix(dataset)
-# pyplot.show()
-
 # split out validation dataset
 array = dataset.values
 x = array[:,0:4]
